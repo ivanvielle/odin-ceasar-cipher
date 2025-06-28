0e0eaa1b1f9625457c69be3b97a6d498ffac4fbf
Ivanvielle Dupaya <65750751+ivanvielle@users.noreply.github.com>
2025-06-28 20:34:49 +0800

    Added comments

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,17 +1,22 @@
 def ceasar_cipher(string,shift)
   results = []
+  # Split strings into comma separated array elements
   words = string.downcase.split
+  # Alphabet range converted to array
   alphabet = ("a"..."z").to_a
 
   words.map {|word|
     letters_array = []
 
+    # Iterate words and break them down into individual letters
     word.chars.map {|letter|
       letter_index = alphabet.find_index(letter)
+      # Cipher letters into their equivalent alphabet index plus n (number of shift)
       ciphered_letter = alphabet[letter_index + shift]
       letters_array.push(ciphered_letter)
     }
 
+    # Join letters and push them into the results array
     results << letters_array.join
   }
 
